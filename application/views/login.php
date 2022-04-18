872748a9fc6444161d9114b252df64335bf7a062
EJaureguiP <emmanuel.jauregui@gmail.com>
2022-04-18 15:08:39 -0700

    upload favicon

--- application/views/login.php
+++ application/views/login.php
@@ -10,6 +10,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/img/favicon.ico">
+
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
